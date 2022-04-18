9c4b50a25f03b7a9875b7ccc477278f8170e0d40
Nisha Chandramoorthy <nishac@mit.edu>
2022-04-18 18:01:45 -0400

    Add absolute difference of all 4 observables.

--- utils/plot_abs_diff_tmsrs.py
+++ utils/plot_abs_diff_tmsrs.py
@@ -35,7 +35,7 @@ for i in range(n_dpts):
         m_dloss += dlosses[k]/n_data
         k = k + 1
 err_dloss = sum((dlosses - m_dloss)**2, axis=0)/n_data
-err_dloss = sqrt(err_dloss)
+err_dloss = sqrt(err_dloss/n_data)
 ax.errorbar(range(num_el)[::skip], m_dloss[::skip], yerr=err_dloss[::skip], fmt="o", ms=6, ecolor="dodgerblue", color="midnightblue", elinewidth=0.4, label='Stoc stab')
 ax.legend(fontsize=16)
 tight_layout()
@@ -68,69 +68,68 @@ for i in range(n_dpts):
         m_dacc += dacces[k]/n_data
         k = k + 1
 err_dacc = sum((dacces - m_dacc)**2, axis=0)/n_data
-err_dacc = sqrt(err_dacc)
+err_dacc = sqrt(err_dacc/n_data)
 ax.errorbar(range(0,dacc*nacc,dacc)[::skip], m_dacc[::skip], yerr=err_dacc[::skip], fmt="o", ms=6, ecolor="dodgerblue", color="midnightblue", elinewidth=2.0, label='Stoc stab')
 ax.legend(fontsize=16)
 tight_layout()
 fig.savefig('../plots/acc_sens_orig.png')
 
 
-
-
-"""
 fig, ax = subplots()
 ax.set_xlabel("Time", fontsize=16)
-ax.set_ylabel("Test accuracy %", fontsize=16)
+ax.set_ylabel(r"$|\Delta $ ||1st lr wt|| |", fontsize=16)
 ax.xaxis.set_tick_params(labelsize=16)
 ax.yaxis.set_tick_params(labelsize=16)
 ax.grid(True)
-for i, di in enumerate(direc):
-    acc = loadtxt('../'+di+'/test_acc.txt')
-    acc = 100*acc[racc:-1]
-    lacc = len(acc)
-    acc = cumsum(acc)/arange(1,lacc+1)
-    timeacc = range(racc*dacc, (racc+lacc)*dacc, dacc)
-    if i < len(direc)-1:
-        ax.plot(timeacc, acc, lw=3.0,label='i/p pert at ind {}'.format(i))
-    else:
-        ax.plot(timeacc, acc,lw=3.0, label='orig, first pt rmvd')
-ax.legend(fontsize=16)
-tight_layout()
-fig.savefig('../plots/test_acc_perts.png')
-
-fig, ax = subplots()
-ax.set_xlabel("Time", fontsize=16)
-ax.set_ylabel("Norm of first layer weights", fontsize=16)
-ax.xaxis.set_tick_params(labelsize=16)
-ax.yaxis.set_tick_params(labelsize=16)
-ax.grid(True)
+nes = zeros((n_dpts, num_el))
 for i, di in enumerate(direc):
     n = loadtxt('../'+di+'/norm.txt')
     n = n[runup:]
-    n = cumsum(n)/arange(1,num_el+1)
-    if i < len(direc)-1:
-        ax.plot(n,lw=3.0,label='i/p pert at ind {}'.format(i))
-    else:
-        ax.plot(n,lw=3.0,label='orig, first pt rmvd')
+    n = cumsum(n)/arange(1, num_el+1)
+    nes[i] = n
+
+k = 0
+dnes = zeros((n_data, num_el))
+m_dn = zeros(num_el)
+for i in range(n_dpts):
+    for j in range(i+1, n_dpts):
+        dnes[k] = abs(nes[i] - nes[j])
+        m_dn += dnes[k]/n_data
+        k = k + 1
+err_dn = sum((dnes - m_dn)**2, axis=0)/n_data
+err_dn = sqrt(err_dn/n_data)
+ax.errorbar(range(num_el)[::skip], m_dn[::skip], yerr=err_dn[::skip], fmt="o", ms=6, ecolor="dodgerblue", color="midnightblue", elinewidth=0.4, label='Stoc stab')
 ax.legend(fontsize=16)
 tight_layout()
-fig.savefig('../plots/weight_norm_perts.png')
+fig.savefig('../plots/norm_sens_orig.png')
+
 
 fig, ax = subplots()
 ax.set_xlabel("Time", fontsize=16)
-ax.set_ylabel("First component of weight vector", fontsize=16)
+ax.set_ylabel(r"$|\Delta $ w[0] |", fontsize=16)
 ax.xaxis.set_tick_params(labelsize=16)
 ax.yaxis.set_tick_params(labelsize=16)
 ax.grid(True)
+ces = zeros((n_dpts, num_el))
 for i, di in enumerate(direc):
     c = loadtxt('../'+di+'/norm_comp.txt')
     c = c[runup:]
-    c = cumsum(c)/arange(1,num_el+1)
-    if i < len(direc)-1:
-        ax.plot(c,lw=3.0,label='i/p pert at ind {}'.format(i))
-    else:
-        ax.plot(c,lw=3.0,label='orig, first pt rmvd')
+    c = cumsum(c)/arange(1, num_el+1)
+    ces[i] = c
+
+k = 0
+dces = zeros((n_data, num_el))
+m_dc = zeros(num_el)
+for i in range(n_dpts):
+    for j in range(i+1, n_dpts):
+        dces[k] = abs(ces[i] - ces[j])
+        m_dc += dces[k]/n_data
+        k = k + 1
+err_dc = sum((dces - m_dc)**2, axis=0)/n_data
+err_dc = sqrt(err_dc/n_data)
+ax.errorbar(range(num_el)[::skip], m_dc[::skip], yerr=err_dc[::skip], fmt="o", ms=6, ecolor="dodgerblue", color="midnightblue", elinewidth=0.4, label='Stoc stab')
 ax.legend(fontsize=16)
 tight_layout()
-fig.savefig('../plots/weight_comp_perts.png')
-"""
+fig.savefig('../plots/comp_sens_orig.png')
+
+
